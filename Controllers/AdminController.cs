aa46b90f3be37d31009858c591507ca002eb277f
onurkybsi <onurbpm@outlook.com>
2020-07-10 10:12:19 +0300

    User delete feature added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -34,7 +34,8 @@ namespace PrototypingASPNETCoreIdentity.Controllers
 
                 var createUserResult = await _userManager.CreateAsync(_newUser, newUser.Password);
 
-                if (createUserResult.Succeeded) return RedirectToAction("Index");
+                if (createUserResult.Succeeded) 
+                    return RedirectToAction("Index");
                 else
                 {
                     SetIdentityErrorsToModelState(createUserResult.Errors);
@@ -42,6 +43,32 @@ namespace PrototypingASPNETCoreIdentity.Controllers
             }
 
             return View(newUser);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+            if(user != null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    SetIdentityErrorsToModelState(result.Errors);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Error", "User Not Found");
+            }
+
+            return View("Index", _userManager.Users);
         }
 
         private void SetIdentityErrorsToModelState(IEnumerable<IdentityError> identityErrors)
