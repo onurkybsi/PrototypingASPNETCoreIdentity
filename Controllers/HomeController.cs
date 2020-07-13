fc74db601ff3571981c753771224ef04f30a2364
onurkybsi <onurbpm@outlook.com>
2020-07-14 01:13:58 +0300

    Login & logout part completed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace PrototypingASPNETCoreIdentity.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public ViewResult Index() => View();
+        public ViewResult Index() => View(GetUserData());
+
+        private Dictionary<string, string> GetUserData()
+        {
+            return new Dictionary<string, string>
+            {
+                ["Username"] = HttpContext.User.Identity.Name,
+                ["IsAuthenticated"] = HttpContext.User.Identity.IsAuthenticated ? "Login successful!" : "Login failed!"
+            };
+        }
     }
 }
